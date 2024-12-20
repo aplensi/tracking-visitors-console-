cee904da0f9b20ea0dde463da8ef6336190bc88c
aplensi <romashev.n16@gmail.com>
2024-12-20 19:29:59 +0300

    added some events and methods for working with time, visitors and places

---
+++ tracking-visitors.cpp
@@ -0,0 +1,169 @@
+﻿#include "tracking-visitors.h"
+
+int main()
+{
+	actionsWithVisitors act;
+	ifstream stream("file.txt");
+	string str;
+	getline(stream, str);
+	if (!act.isPlace(str))
+	{
+		cout << "line of places is skipped";
+		return 0;
+	}
+	act.setPlaces(atoi(str.c_str()));
+	getline(stream, str);
+	if (!act.isOpenTime(str))
+	{
+		cout << "line of time is skipped";
+		return 0;
+	}
+	getline(stream, str);
+	if (atoi(str.c_str()) < 0)
+	{
+		cout << "line of price is skipped";
+		return 0;
+	}
+	else
+	{
+		act.price = atoi(str.c_str());
+	}
+
+	while (getline(stream, str))
+	{
+		//cout << str << endl;
+		act.event(str);
+	}
+	for (const auto& i : act.vecVisitors)
+	{
+		cout << i.name << endl;
+	}
+
+	return 0;
+}
+
+void actionsWithVisitors::event(string s)
+{
+	cout << s << endl;
+	stringstream ss(s);
+	int time, status, idPlace;
+	string name, sTime, sStatus, sIdPlace;
+	int countW = countWords(s);
+	if (countW == 3)
+	{
+		ss >> sTime >> sStatus >> name;
+	}
+	else {
+		ss >> sTime >> sStatus >> name >> sIdPlace;
+	}
+	time = hoursToMinuts(sTime);
+	status = atoi(sStatus.c_str());
+	idPlace = atoi(sIdPlace.c_str());
+
+	if (time < hoursToMinuts(openTime))
+	{
+		cout << sTime << " 13 NotOpenYet " << endl;
+		return;
+	}
+	if (status == 1)
+	{
+		visitors visitor;
+		visitor.timeOfEntry = sTime;
+		visitor.name = name;
+		visitor.status = status;
+		vecVisitors.push_back(visitor);
+	}
+}
+
+bool actionsWithVisitors::isPlace(string s)
+{
+	if (!isNumber(s))
+	{
+		return 0;
+	}
+	if (atoi(s.c_str()) < 0)
+	{
+		return 0;
+	}
+	countOfPlaces = atoi(s.c_str());
+	return 1;
+}
+
+bool actionsWithVisitors::isOpenTime(string s)
+{
+	if (isTime(0, s) == "" || isTime(6, s) == "")
+	{
+		return false;
+	}
+	openTime = s.substr(0, 5);
+	closeTime = s.substr(6, 5);
+	return true;
+}
+
+string actionsWithVisitors::isTime(int startOfTime, string s)
+{
+	string h = s.substr(0 + startOfTime, 2);
+	string m = s.substr(3 + startOfTime, 2);
+	if (!isNumber(h) || atoi(h.c_str()) < 0 || atoi(h.c_str()) > 24)
+	{
+		return "";
+	}
+	if (!isNumber(m) || atoi(m.c_str()) < 0 || atoi(m.c_str()) > 60)
+	{
+		return "";
+	}
+	string time = s.substr(0 + startOfTime, 5);
+	return time;
+}
+
+int actionsWithVisitors::hoursToMinuts(string s) {
+	int hours, minutes;
+	char colon;
+
+	std::stringstream ss(s);
+
+	if (ss >> hours >> colon >> minutes && colon == ':') {
+		return hours * 60 + minutes;
+	}
+	else {
+		std::cerr << "Ошибка: некорректный формат времени." << std::endl;
+		return -1;
+	}
+}
+
+void actionsWithVisitors::setPlaces(int)
+{
+	for (int i = 0; i < countOfPlaces; i++) {
+		vecTables.push_back({ 0, false });
+	}
+}
+
+string actionsWithVisitors::minutesToHours(int totalMinutes)
+{
+	int hours = totalMinutes / 60;
+	int minutes = totalMinutes % 60;
+
+	char buffer[6];
+	snprintf(buffer, sizeof(buffer), "%02d:%02d", hours, minutes);
+
+	return std::string(buffer);
+}
+
+bool actionsWithVisitors::isNumber(const std::string& s)
+{
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && std::isdigit(*it)) ++it;
+	return !s.empty() && it == s.end();
+}
+
+int actionsWithVisitors::countWords(string str)
+{
+	istringstream iss(str);
+	string word;
+	int count = 0;
+	while (iss >> word) {
+		count++;
+	}
+
+	return count;
+}
---
+++ tracking-visitors.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct visitors
+{
+	string timeOfEntry;
+	string exitTime;
+	string startTime;
+	string name;
+	int timeUsed, status;
+};
+
+struct tables
+{
+	int workTime = 0;
+	bool status = false;
+};
+
+class actionsWithVisitors
+{
+public:
+	void event(string);
+	bool isPlace(string);
+	bool isOpenTime(string);
+	int hoursToMinuts(string);
+	void setPlaces(int);
+	string minutesToHours(int);
+	int countOfPlaces;
+	int price;
+	vector<visitors> vecVisitors;
+private:
+	string openTime, closeTime;
+	string isTime(int startOfTime, string);
+	bool isNumber(const std::string&);
+	int countWords(string);
+	
+	vector<tables> vecTables;
+};
